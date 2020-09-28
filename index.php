76752721a9dc0c36ed7e7056eca07c7efd7f940a
Kurt Mac <campherkurt2@gmail.com>
2020-09-28 16:16:51 +0200

    Aded initial curl request for slack message

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <<?php
-echo "hey";
+
+$postRequest = array(
+    'channel' => 'C01B31YQM39',
+    'text' => 'hello world :tada:'
+);
+
+$headers =  array(
+    'Content-type: application/json; charset=UTF-8',
+    'Authorization: Bearer xoxb-1391806062546-1398022628996-E7xlt2cFSpZo79iDCtqVWoag'
+);
 
 
+$cURLConnection = curl_init('https://slack.com/api/chat.postMessage');
+curl_setopt($cURLConnection, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, json_encode($postRequest));
+curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+
+$apiResponse = curl_exec($cURLConnection);
+curl_close($cURLConnection);
+
+// $apiResponse - available data from the API request
+//$jsonArrayResponse = json_decode($apiResponse);
+
+echo 'Response found: <br/>';
+echo $apiResponse;
 
  ?>
