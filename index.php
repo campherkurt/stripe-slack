941e7c510f3e72d60f0645ae7d8a9285b8c301d9
Kurt Mac <campherkurt2@gmail.com>
2020-09-28 16:24:06 +0200

    Removed channel and auth token

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <<?php
 
 $postRequest = array(
-    'channel' => 'C01B31YQM39',
+    'channel' => '<Add channel ID here>',
     'text' => 'hello world :tada:'
 );
 
 $headers =  array(
     'Content-type: application/json; charset=UTF-8',
-    'Authorization: Bearer xoxb-1391806062546-1398022628996-E7xlt2cFSpZo79iDCtqVWoag'
+    'Authorization: Bearer <Add OAuth token here'
 );
 
 
